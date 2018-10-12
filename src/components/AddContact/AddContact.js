e1e99c43c04699f8d1ef3902c8eb3b8097ec7d69
Hubert Wiśniewski <hubert_w@autograf.pl>
2018-10-12 20:27:43 +0200

    Its functional

--- src/components/AddContact/AddContact.js
+++ src/components/AddContact/AddContact.js
@@ -1,16 +1,67 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class AddContact extends Component {
+  state = {
+    contactName: "",
+    contactNumber: '',
+    error: null
+  };
 
-render() {
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.contactName === "") {
+      this.setState({
+        error: new Error("You need to come up with a name")
+      });
+      return;
+    }
+    if (this.state.contactNumber === "") {
+        this.setState({
+          error: new Error("You need to provide a number")
+        });
+        return;
+      } 
+    if (isNaN(this.state.contactNumber)) {
+        this.setState({
+            error: new Error("You need to provide a number")
+          });
+          return;
+    }  
+    this.props.addContact(this.state.contactName, this.state.contactNumber);
+    this.setState({ contactName: "", contactNumber: '', error: null });
+  };
+
+  handleNameChange = event => {
+    this.setState({
+      contactName: event.target.value
+    });
+  };
+
+  handleNumberChange = event => {
+    this.setState({
+      contactNumber: event.target.value
+    });
+  };
 
 
-
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {this.state.error && <p>{this.state.error.message}</p>}
+        <input
+          placeholder="Add contact name"
+          value={this.state.contactName}
+          onChange={this.handleNameChange}
+        />
+         <input
+          placeholder="Add contact number"
+          value={this.state.contactNumber}
+          onChange={this.handleNumberChange}
+        />
+        <button>Add</button>
+      </form>
+    );
+  }
 }
 
-
-
-
-}
-
-export default AddContact
+export default AddContact;
--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import AddContact from "./AddContact/AddContact";
 
 class App extends Component {
-  state = { contacts: [
-    {
-      id: 1,
-      name: 'Andrzej',
-      number: '255439029'
-    },
-    {
-      id: 2,
-      name: 'Dżon',
-      number: '888777222'
-    },
-    {
-      id: 3,
-      name: 'Wisława',
-      number: '444938092'
-    }
-  ] }
 
-    removeContact = contactId => {
-      this.setState({
-        contacts: this.state.contacts.filter(
-          contact => contactId !== contact.id
-        )
+  state = {
+    contacts: JSON.parse(localStorage.getItem('contacts') || '[]'),
+    previousState: null
+  }
+
+  removeContact = contactId => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contactId !== contact.id)
+    });
+  };
+
+  addContact = (name, number) => {
+    this.setState({
+      previousState: this.state,
+      contacts: this.state.contacts.concat({
+        id: Date.now(),
+        name: name,
+        number: number     
       })
-    }
+    })
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  }
 
   render() {
     return (
       <div className="App">
         <h1>List of people I might call someday</h1>
-
+        <AddContact addContact={this.addContact}/>
         <ul>
-          {
-            this.state.contacts.map( contact => (
-              <li key={contact.id}>{contact.name}      {contact.number}
-              
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name} {contact.number}
               {
-                <button onClick={() => this.removeContact(contact.id)}>Remove</button>
+                <button onClick={() => this.removeContact(contact.id)}>
+                  Remove
+                </button>
               }
-              </li>
-            )
-          )
-
-
-          }
-
+            </li>
+          ))}
         </ul>
-
       </div>
     );
   }
